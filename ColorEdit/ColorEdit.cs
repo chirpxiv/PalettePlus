15fe9cd3ef78e3eef6591bfc6bed67ee9620976a
chirp <ch1rp@protonmail.com>
2023-01-07 02:14:22 +0000

    Implement Configuration

--- ColorEdit/ColorEdit.cs
+++ ColorEdit/ColorEdit.cs
@@ -5,14 +5,19 @@ using Dalamud.Game.Command;
 using ColorEdit.Interop;
 using ColorEdit.Interface;
 using ColorEdit.Interface.Windows;
+using Dalamud.Logging;
 
 namespace ColorEdit {
 	public sealed class ColorEdit : IDalamudPlugin {
 		public string Name => "ColorEdit";
 		public string CommandName = "/coloredit";
 
+		public static Configuration Config { get; internal set; } = null!;
+
 		public ColorEdit(DalamudPluginInterface dalamud) {
 			Services.Init(dalamud);
+
+			Configuration.LoadConfig();
 
 			Hooks.Init();
 
@@ -30,10 +35,14 @@ namespace ColorEdit {
 			Services.Interface.UiBuilder.Draw -= PluginGui.Windows.Draw;
 
 			Services.CommandManager.RemoveHandler(CommandName);
+
+			Services.Interface.SavePluginConfig(Config);
 		}
 
-		private void OnCommand(string _, string arguments) {
-			PluginGui.GetWindow<MainWindow>().Show();
-		}
+		private void OnCommand(string _, string arguments)
+			=> PluginGui.GetWindow<MainWindow>().Show();
+
+		internal static string GetVersion()
+			=> typeof(ColorEdit).Assembly.GetName().Version!.ToString(fieldCount: 3);
 	}
 }
---
+++ ColorEdit/Configuration.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Dalamud.Configuration;
+using Dalamud.Logging;
+
+using ColorEdit.Palettes;
+using ColorEdit.Interface;
+using ColorEdit.Interface.Windows;
+
+namespace ColorEdit {
+	[Serializable]
+	public class Configuration : IPluginConfiguration {
+		public const int _ConfigVer = 0;
+
+		public int Version { get; set; } = _ConfigVer;
+		public string PluginVersion { get; set; } = ColorEdit.GetVersion();
+
+		public bool IsFirstTime { get; set; } = true;
+
+		public Dictionary<string, Palette> SavedPalettes { get; set; } = new();
+
+		public static void LoadConfig() {
+			try {
+				ColorEdit.Config = Services.Interface.GetPluginConfig() as Configuration ?? new();
+			} catch(Exception e) {
+				PluginLog.Error("Failed to load ColorEdit config. Settings have been reset.", e);
+				ColorEdit.Config = new();
+			}
+			ColorEdit.Config.Init();
+		}
+
+		public void Init() {
+			if (Version != _ConfigVer)
+				Upgrade();
+
+			var curVer = ColorEdit.GetVersion();
+			if (PluginVersion != curVer) {
+				// TODO: Changelog?
+				PluginVersion = curVer;
+			}
+
+			if (IsFirstTime) {
+				IsFirstTime = false;
+				PluginGui.GetWindow<MainWindow>().Show();
+			}
+		}
+
+		private void Upgrade() {
+			PluginLog.Warning(string.Format(
+				"Upgrading config version from {0} to {1}.\nThis is nothing to worry about, but some settings may change or reset!",
+				Version, _ConfigVer
+			));
+
+			Version = _ConfigVer;
+		}
+	}
+}
