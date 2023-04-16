c3e1ee56858db72a986545d38438507201fcf094
chirp <ch1rp@protonmail.com>
2023-04-16 09:54:14 +0100

    Add Sqrt/Pow2 extensions for Vectors

---
+++ PalettePlus/Extensions/Vector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+namespace PalettePlus.Extensions {
+	internal static class VectorExtensions {
+		internal static void RgbSqrt(this Vector3 vec) {
+			vec.X = (float)Math.Sqrt(vec.X);
+			vec.Y = (float)Math.Sqrt(vec.Y);
+			vec.Z = (float)Math.Sqrt(vec.Z);
+		}
+		
+		internal static void RgbSqrt(this Vector4 vec) {
+			vec.X = (float)Math.Sqrt(vec.X);
+			vec.Y = (float)Math.Sqrt(vec.Y);
+			vec.Z = (float)Math.Sqrt(vec.Z);
+		}
+
+		internal static void RgbPow2(this Vector3 vec) {
+			vec.X *= vec.X;
+			vec.Y *= vec.Y;
+			vec.Z *= vec.Z;
+		}
+		
+		internal static void RgbPow2(this Vector4 vec) {
+			vec.X *= vec.X;
+			vec.Y *= vec.Y;
+			vec.Z *= vec.Z;
+		}
+	}
+}
