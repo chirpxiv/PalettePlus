c4e5b4a2da95c1cd0ae68c9300df440ca20dc7f4
chirp <72366111+chirpxiv@users.noreply.github.com>
2023-08-21 22:02:21 +0100

    Implement CharaModel struct

---
+++ PalettePlus/Interop/Structs/CharaModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
+
+namespace PalettePlus.Interop.Structs; 
+
+[StructLayout(LayoutKind.Explicit)]
+public struct CharaModel {
+	[FieldOffset(0)] private Human Inner;
+
+	[FieldOffset(0x9D8)] public unsafe ConstBuffer<ModelParams>* ModelBuffer;
+	[FieldOffset(0x9E0)] public unsafe ConstBuffer<DecalParams>* DecalBuffer;
+
+	public unsafe ModelParams* ModelParams => this.ModelBuffer != null ? this.ModelBuffer->Data : null;
+	public unsafe DecalParams* DecalParams => this.DecalBuffer != null ? this.DecalBuffer->Data : null;
+}
