635d6db9435ed2df5a2bfd0597ccafc179683f91
chirp <72366111+chirpxiv@users.noreply.github.com>
2023-08-22 00:48:44 +0100

    Add sigs for `TextureUpload`, `TextureCreate2D`
    Co-Authored-By: Exter-N <exter-n@exter-n.fr>

--- PalettePlus/Interop/InteropService.cs
+++ PalettePlus/Interop/InteropService.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 
+using Dalamud.Utility.Signatures;
+
+using FFXIVClientStructs.FFXIV.Client.Graphics.Kernel;
+using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
+
 using PalettePlus.Core;
 using PalettePlus.Interop.Hooking;
 
 namespace PalettePlus.Interop;
 
-// TODO: Delegate declarations
+// Delegate declarations
+
+public unsafe delegate bool TextureUploadDelegate(Texture* texture, nint contents);
+public unsafe delegate Texture* TextureCreate2DDelegate(Device* device, int* size, byte mipLevel, uint texFormat, uint flags, uint unk);
 
 // ReSharper disable once ClassNeverInstantiated.Global
 public class InteropService : IDisposable {
@@ -14,11 +22,18 @@ public class InteropService : IDisposable {
 	private readonly HookManager Hooks;
 	
 	// Native functions
+
+	[Signature("E9 ?? ?? ?? ?? 8B 02 25")]
+	public TextureUploadDelegate TextureUpload = null!;
+
+	[Signature("E8 ?? ?? ?? ?? 8B 0F 48 8D 54 24")]
+	public TextureCreate2DDelegate TextureCreate2D = null!;
 	
 	// Service initialization
 	
 	public InteropService(IServiceContainer _services) {
 		this.Hooks = _services.Inject<HookManager>();
+		SignatureHelper.Initialise(this);
 	}
 	
 	// Disposal
