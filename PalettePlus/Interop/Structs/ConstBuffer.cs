5811f1612b9921843a8fe7cecd98beba619c0429
chirp <72366111+chirpxiv@users.noreply.github.com>
2023-08-21 20:56:45 +0100

    Introduce generic parameter for `ConstBuffer`

--- PalettePlus/Interop/Structs/ConstBuffer.cs
+++ PalettePlus/Interop/Structs/ConstBuffer.cs
@@ -1,10 +1,18 @@
 ﻿using System.Runtime.InteropServices;
 
-namespace PalettePlus.Interop.Structs; 
+namespace PalettePlus.Interop.Structs;
 
 [StructLayout(LayoutKind.Explicit, Size = 0x70)]
 public struct ConstBuffer {
 	[FieldOffset(0x00)] public nint __vtable;
-	[FieldOffset(0x28)] public nint Values;
+	[FieldOffset(0x28)] public nint Address;
 	[FieldOffset(0x50)] public unsafe fixed ulong __unkPtr[4];
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct ConstBuffer<T> where T : unmanaged {
+	private ConstBuffer Inner;
+
+	public nint Address => this.Inner.Address;
+	public unsafe T* Data => (T*)this.Inner.Address;
 }
