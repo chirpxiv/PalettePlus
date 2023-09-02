e71c7451c860ec5d71e685da860557107225119c
Exter-N <exter-n@exter-n.fr>
2023-09-02 20:18:00 +0200

    Improve HDR color handling

--- PalettePlus/Extensions/Vector.cs
+++ PalettePlus/Extensions/Vector.cs
@@ -3,25 +3,35 @@ using System.Numerics;
 
 namespace PalettePlus.Extensions {
 	internal static class VectorExtensions {
+		private static float RgbSqrt(float x)
+			=> x < 0.0f ? -MathF.Sqrt(-x) : MathF.Sqrt(x);
+
 		internal static Vector3 RgbSqrt(this Vector3 vec) => new(
-			(float)Math.Sqrt(vec.X),
-			(float)Math.Sqrt(vec.Y),
-			(float)Math.Sqrt(vec.Z)
+			RgbSqrt(vec.X),
+			RgbSqrt(vec.Y),
+			RgbSqrt(vec.Z)
 		);
 
 		internal static Vector4 RgbSqrt(this Vector4 vec) => new(
-			(float)Math.Sqrt(vec.X),
-			(float)Math.Sqrt(vec.Y),
-			(float)Math.Sqrt(vec.Z),
+			RgbSqrt(vec.X),
+			RgbSqrt(vec.Y),
+			RgbSqrt(vec.Z),
 			vec.W
 		);
 
-		internal static Vector3 RgbPow2(this Vector3 vec) => vec * vec;
+		private static float RgbPow2(float x)
+			=> x < 0.0f ? -(x * x) : x * x;
+
+		internal static Vector3 RgbPow2(this Vector3 vec) => new(
+			RgbPow2(vec.X),
+			RgbPow2(vec.Y),
+			RgbPow2(vec.Z)
+		);
 
 		internal static Vector4 RgbPow2(this Vector4 vec) => new(
-			vec.X * vec.X,
-			vec.Y * vec.Y,
-			vec.Z * vec.Z,
+			RgbPow2(vec.X),
+			RgbPow2(vec.Y),
+			RgbPow2(vec.Z),
 			vec.W
 		);
 	}
