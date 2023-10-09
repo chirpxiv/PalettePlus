3d63b2116f21a6159fffb3cae2aa3f3749cc8c7a
chirp <72366111+chirpxiv@users.noreply.github.com>
2023-10-09 12:24:38 +0100

    Convert `Hooks` to use `SignatureAttribute`

---
+++ PalettePlus/Structs/CharaSetup.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+
+namespace PalettePlus.Structs; 
+
+[StructLayout(LayoutKind.Explicit, Size = 0x15)]
+public struct CharaSetup {
+	[FieldOffset(0x08)] public unsafe Character* Character;
+	[FieldOffset(0x10)] public byte Unknown1;
+	[FieldOffset(0x11)] public uint Unknown2;
+}
