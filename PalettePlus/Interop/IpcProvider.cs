287c629d640e2a6c8f5a12bc88bb6ba4b9b54237
Limiana <5073202+Limiana@users.noreply.github.com>
2023-12-30 03:55:54 +0300

    Add new ipc method

--- PalettePlus/Interop/IpcProvider.cs
+++ PalettePlus/Interop/IpcProvider.cs
@@ -6,6 +6,9 @@ using Dalamud.Game.ClientState.Objects.Types;
 
 using PalettePlus.Services;
 using PalettePlus.Palettes;
+using System.Linq;
+using Newtonsoft.Json;
+using PalettePlus.Extensions;
 
 namespace PalettePlus.Interop {
 	public static class IpcProvider {
@@ -15,20 +18,25 @@ namespace PalettePlus.Interop {
 		public const string GetCharaPaletteStr = "PalettePlus.GetCharaPalette";
 		public const string SetCharaPaletteStr = "PalettePlus.SetCharaPalette";
 		public const string RemoveCharaPaletteStr = "PalettePlus.RemoveCharaPalette";
+		public const string RedrawCharaStr = "PalettePlus.RedrawChara";
 		public const string BuildCharaPaletteStr = "PalettePlus.BuildCharaPalette";
 		public const string BuildCharaPaletteOrEmptyStr = "PalettePlus.BuildCharaPaletteOrEmpty";
 		public const string PaletteChangedStr = "PalettePlus.PaletteChanged";
+		public const string GetSavedPalettesStr = "PalettePlus.GetSavedPalettes";
 
 		private static ICallGateProvider<string>? IApiVersion;
 
 		private static ICallGateProvider<Character, string>? IGetCharaPalette;
 		private static ICallGateProvider<Character, string, object>? ISetCharaPalette;
 		private static ICallGateProvider<Character, object>? IRemoveCharaPalette;
+		private static ICallGateProvider<Character, object>? IRedrawChara;
 		private static ICallGateProvider<Character, string, object>? IPaletteChanged;
 
 		// Constructs a new Palette object from character memory instead of fetching from cache.
 		private static ICallGateProvider<Character, string>? IBuildCharaPalette;
 		private static ICallGateProvider<Character, string>? IBuildCharaPaletteOrEmpty;
+				
+		private static ICallGateProvider<string[]>? IGetSavedPalettes;
 
 		internal static void Init() {
 			try {
@@ -44,6 +52,9 @@ namespace PalettePlus.Interop {
 				IRemoveCharaPalette = PluginServices.Interface.GetIpcProvider<Character, object>(RemoveCharaPaletteStr);
 				IRemoveCharaPalette.RegisterAction(RemoveCharaPalette);
 
+				IRedrawChara = PluginServices.Interface.GetIpcProvider<Character, object>(RedrawCharaStr);
+				IRedrawChara.RegisterAction(RedrawChara);
+
 				IBuildCharaPalette = PluginServices.Interface.GetIpcProvider<Character, string>(BuildCharaPaletteStr);
 				IBuildCharaPalette.RegisterFunc(BuildCharaPalette);
 
@@ -51,6 +62,9 @@ namespace PalettePlus.Interop {
 				IBuildCharaPaletteOrEmpty.RegisterFunc(BuildCharaPaletteOrEmpty);
 
 				IPaletteChanged = PluginServices.Interface.GetIpcProvider<Character, string, object>(PaletteChangedStr);
+								
+				IGetSavedPalettes = PluginServices.Interface.GetIpcProvider<string[]>(GetSavedPalettesStr);
+        IGetSavedPalettes.RegisterFunc(GetSavedPalettes);
 			} catch (Exception e) {
 				PluginServices.Log.Error("Failed to initialise Palette+ IPC", e);
 			}
@@ -61,8 +75,10 @@ namespace PalettePlus.Interop {
 			IGetCharaPalette?.UnregisterFunc();
 			ISetCharaPalette?.UnregisterAction();
 			IRemoveCharaPalette?.UnregisterAction();
+			IRedrawChara?.UnregisterAction();
 			IBuildCharaPalette?.UnregisterFunc();
 			IBuildCharaPaletteOrEmpty?.UnregisterFunc();
+			IGetSavedPalettes?.UnregisterFunc();
 		}
 
 		// IPC Methods
@@ -78,7 +94,12 @@ namespace PalettePlus.Interop {
 		private static void RemoveCharaPalette(Character chara)
 			=> PaletteService.RemoveCharaPalette(chara);
 
-		private static string BuildCharaPalette(Character chara) {
+		private unsafe static void RedrawChara(Character actor) {
+        actor.Redraw();
+    }
+
+
+    private static string BuildCharaPalette(Character chara) {
 			PaletteService.BuildCharaPalette(chara, out var palette, out _, false);
 			return palette.ToString();
 		}
@@ -89,7 +110,12 @@ namespace PalettePlus.Interop {
 			return palette.ToString();
 		}
 
-		internal static void PaletteChanged(Character character, Palette? palette) {
+		private static string[] GetSavedPalettes() {
+				return PalettePlus.Config.SavedPalettes.Select(JsonConvert.SerializeObject).ToArray();
+		}
+
+
+    internal static void PaletteChanged(Character character, Palette? palette) {
 			IPaletteChanged?.SendMessage(character, palette == null ? string.Empty : palette.ToString());
 		}
 
